34c3f86bc4541f0da7c089cfecd945274db6b371
Shafique Ahmed <shafiqued17z@gmail.com>
2021-07-28 02:20:49 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sampleviewpager/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.sampleviewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    TabItem tabItem1, tabItem2, tabItem3;
+    ViewPager viewPager;
+    PageAdapter pageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        tabItem1 = findViewById(R.id.chat_tab);
+        tabItem2 = findViewById(R.id.status_tab);
+        tabItem3 = findViewById(R.id.call_tab);
+        viewPager = findViewById(R.id.view_pager);
+
+        pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                if (tab.getPosition() == 0 || tab.getPosition() == 1 || tab.getPosition() == 2)
+                    pageAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        //listens for scroll or page change.
+
+    }
+}
